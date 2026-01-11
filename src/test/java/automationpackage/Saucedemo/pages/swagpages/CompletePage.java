8099a2c61fcbe254f7c16700c121e797823b7ba1
madhava4747 <95749543+madhava4747@users.noreply.github.com>
2026-01-11 22:34:17 +0530

    Update CompletePage.java

--- src/test/java/automationpackage/Saucedemo/pages/swagpages/CompletePage.java
+++ src/test/java/automationpackage/Saucedemo/pages/swagpages/CompletePage.java
@@ -7,6 +7,8 @@ import automationpackage.Saucedemo.utils.TestLogCollector;
 public class CompletePage extends BasePage {
 
     private final By successMsg = By.className("complete-header");
+    private final By errorMsg = By.cssSelector("[data-test='error']");
+    
 
     public CompletePage(WebDriver driver) {
         super(driver);
@@ -17,4 +19,10 @@ public class CompletePage extends BasePage {
         TestLogCollector.log("INFO", "Order success message: " + msg);
         return msg;
     }
+    public boolean isSucessErrorDisplayed() {
+		boolean displayed = driver.findElements(errorMsg).size() > 0;
+        TestLogCollector.log("INFO", "Success error displayed: " + displayed);
+		// TODO Auto-generated method stub
+		return displayed;
+	}
 }
