31d8c4b94d7a2ac96a3e75c0c625c788215c631b
madhava4747 <95749543+madhava4747@users.noreply.github.com>
2026-01-11 22:33:29 +0530

    Update CartPage.java

--- src/test/java/automationpackage/Saucedemo/pages/swagpages/CartPage.java
+++ src/test/java/automationpackage/Saucedemo/pages/swagpages/CartPage.java
@@ -8,6 +8,8 @@ public class CartPage extends BasePage {
 
     private final By cartTitle = By.className("title");
     private final By checkoutBtn = By.id("checkout");
+    private final By errorMsg = By.cssSelector("[data-test='error']");
+    
 
     public CartPage(WebDriver driver) {
         super(driver);
@@ -23,4 +25,11 @@ public class CartPage extends BasePage {
         TestLogCollector.log("INFO", "Clicking Checkout button");
         click(checkoutBtn);
     }
+
+	public boolean isEmptyCartErrorDisplayed() {
+		boolean displayed = driver.findElements(errorMsg).size() > 0;
+        TestLogCollector.log("INFO", "Cart Page error displayed: " + displayed);
+		// TODO Auto-generated method stub
+		return displayed;
+	}
 }
